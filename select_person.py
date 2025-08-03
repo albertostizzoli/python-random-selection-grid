dbb3a5d9119c157e9e6d986c6bd6208ea8cdcfb0
albertostizzoli <albertostizzoli@yahoo.it>
2025-08-03 17:52:19 +0200

    create loop for found names in grid

--- select_person.py
+++ select_person.py
@@ -28,6 +28,22 @@ class App:
         
         self.frame_grid = tk.Frame(root)  # Frame per la griglia
         self.frame_grid.pack()
-        
+
+        # Ciclo per creare le etichette nelle righe della griglia
+        for i in range(ROWS):
+            row = [] # Lista per la riga corrente
+
+            # Ciclo per creare le etichette nelle colonne della griglia
+            for j in range(COLUMNS):
+                idx = i * COLUMNS + j # Calcolo l'indice del nome
+                name = names[idx] if idx < len(names) else "" # Ottengo il nome corrispondente
+                lbl = tk.Label(self.frame_grid, text=name, width=20, height=2, relief="solid", bg="lightblue", font=("Arial", 12)) # Crea l'etichetta
+                lbl.grid(row= i, column=j, padx=5, pady=5) # Posiziona l'etichetta nella griglia
+                row.append(lbl) # Aggiungo l'etichetta alla riga corrente
+            self.labels.append(row) # Aggiungo la riga alla lista delle etichette
+
+        self.btn = tk.Button(root, text="🎲 Seleziona", command=self.start_animation) # Pulsante per avviare
+        self.btn.pack(pady=10) # Aggiungo il pulsante per avviare la selezione)
+
 
     
