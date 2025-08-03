58ff4aa7ef37c6a6ea0ca98f7d67db8ad9f60ed9
albertostizzoli <albertostizzoli@yahoo.it>
2025-08-03 16:54:26 +0200

    import libraries random and tkinter and wrote first names

---
+++ select_person.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import random
+
+# Lista di nomi e cognomi
+names = [
+    "Alberto Stizzoli", "Mekki Ouertani", "Simone Negrisoli", "Francesco Carrara", "Luca Masera",
+    "Stefano Zidda", "Mirko Marasco", "Vinicius De Miranda", "Oder Risi", "Massimiliano Gilli",
+    "Iacopo de Palatis", 
+]
